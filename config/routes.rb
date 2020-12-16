744b5786f1bcfc28e9ea1e2c77373fa374b9caf6
toshiro-koba <kobato.231.0625@gmail.com>
2020-12-16 19:44:05 +0900

    ユーザー管理用のコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   root to: 'tasks#index'
   resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
