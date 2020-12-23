0073ab3cfdefe913456fda27b1199ec43e64178d
toshiro-koba <kobato.231.0625@gmail.com>
2020-12-23 14:30:25 +0900

    タスク表示確認の記述を共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -15,15 +15,17 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    # ユーザーAが作成したタスクの名称が画面上に表示されていることを確認
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしている時' do
       # ユーザーAでログインする
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -46,10 +48,7 @@ describe 'タスク管理機能', type: :system do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        # 作成済みのタスクの名称が画面上に表示されていることを確認
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
