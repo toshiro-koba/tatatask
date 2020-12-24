d36ef3c2e5eb36fba15b19193fc9bbb745a97d0b
toshiro-koba <kobato.231.0625@gmail.com>
2020-12-24 14:04:07 +0900

    メール送信処理を記述

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,2 +1,12 @@
 class TaskMailer < ApplicationMailer
+  default from: 'taskleaf@example.com'
+
+  def creation_email(task)
+    @task = task
+    mail(
+      subject: 'タスク作成完了メール',
+      to: 'kobato.231.0625@gmail.com',
+      from: 'taskleaf@example.com'
+    )
+  end
 end
